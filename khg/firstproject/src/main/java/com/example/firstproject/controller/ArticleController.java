1f183baa86ed811638f4cb7cedb9fabf2b9d003b
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-11-25 14:37:42 +0900

    feat : 데이터 목록조회

--- khg/firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ khg/firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 
 @Controller
 @Slf4j  // 로깅을 위한 골뱅이 @
@@ -61,7 +63,16 @@ public class ArticleController {
 
         // 3: 보여줄 페이지를 설정!
         return "articles/show";
+    }
 
+    @GetMapping("/articles")
+    public String index(Model model) {
+        // 1: 모든 Article을 가져온다!
+        List<Article> articleEntityList = articleRepository.findAll();
+        // 2: 가져온 Article 묶을을 뷰로 전달!
+        model.addAttribute("articleList", articleEntityList);
+        // 3: 뷰 페이지를 설정!
+        return "articles/index";  // articles/index.mustache
     }
 
 }
--- khg/firstproject/src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ khg/firstproject/src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -3,7 +3,10 @@ package com.example.firstproject.repository;
 import com.example.firstproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
 
-
+    @Override
+    ArrayList<Article> findAll(); // ArrayList롤 반환하기 위한 오버라이딩
 }
